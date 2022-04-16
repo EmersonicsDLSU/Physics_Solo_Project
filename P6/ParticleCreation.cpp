939202ba80d570c2b31eec513494714d942dd054
EmersonicsDLSU <emerson_celestial@dlsu.edu.ph>
2022-04-16 16:14:35 +0800

    Update Particles and Shapes

--- P6/ParticleCreation.cpp
+++ P6/ParticleCreation.cpp
@@ -29,7 +29,7 @@ void ParticleCreation::Creation_Initialize()
 
     // Create a particle
     ParticleStruct particle_struct1 = 
-    {10.0f, MyVector(400, 0),
+    {10.0f, MyVector(100, 0),
     	MyVector(0, 10), MyVector(0, 10)};
     MyParticle* particle1 = new MyParticle(particle_struct1);
     
@@ -38,12 +38,30 @@ void ParticleCreation::Creation_Initialize()
     	MyVector(0, 10), MyVector(0, 10)};
     MyParticle* particle2 = new MyParticle(particle_struct2);
 
+    ParticleStruct particle_struct3 = 
+    {10.0f, MyVector(300, 0),
+    	MyVector(0, 10), MyVector(0, 10)};
+    MyParticle* particle3 = new MyParticle(particle_struct3);
+
+    ParticleStruct particle_struct4 = 
+    {10.0f, MyVector(400, 0),
+    	MyVector(0, 10), MyVector(0, 10)};
+    MyParticle* particle4 = new MyParticle(particle_struct4);
+
     // Create an SFML particle
     SFML_ParticleStruct shape_struct1 = {
     	50.0f, sf::Color::Green, particle1, renderPoint};
     SFML_Particle* SFML_particle1 = new SFML_Particle(shape_struct1);
 
     SFML_ParticleStruct shape_struct2 = {
-    	50.0f, sf::Color::Red, particle2, renderPoint};
+    	50.0f, sf::Color::Yellow, particle2, renderPoint};
     SFML_Particle* SFML_particle2 = new SFML_Particle(shape_struct2);
+    
+    SFML_ParticleStruct shape_struct3 = {
+    	50.0f, sf::Color::Red, particle3, renderPoint};
+    SFML_Particle* SFML_particle3 = new SFML_Particle(shape_struct3);
+
+    SFML_ParticleStruct shape_struct4 = {
+    	50.0f, sf::Color::White, particle4, renderPoint};
+    SFML_Particle* SFML_particle4 = new SFML_Particle(shape_struct4);
 }
